c4952fd85980a4755175192476d118c9ee3484a6
David Noya Couselo <148707872+dnoyacouselo@users.noreply.github.com>
2025-07-14 13:16:33 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+<script>
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
+</script>
